d49bb64ee84ba345fc9e802878d40a2b0a5cc67e
Merca Ovnerud <merca666@gmail.com>
2023-03-28 08:04:01 +0200

    Add environments (#2)

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -10,3 +10,11 @@ variable "deploy_app_object_id" {
   type        = string
   description = "The object ID of the service principal that will deploy the resources"
 }
+variable "environment" {
+  type        = string
+  description = "The environment to deploy to"
+}
+variable "name" {
+  type        = string
+  description = "The name prefix to use for the resources"
+}
